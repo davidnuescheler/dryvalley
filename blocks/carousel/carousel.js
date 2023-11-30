5c883dfc4b33453a7983982a2f5ad2c1ea954ac5
David Nuescheler <david@nuescheler.ch>
2023-11-30 16:32:09 -0700

    chore: add styling and simple carousel

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+    const buttons = document.createElement('div');
+    buttons.className = 'carousel-buttons';
+    [...block.children].forEach((row, i) => {
+      const classes = ['image', 'text'];
+      const children = [...row.children];
+      children.forEach((e, j) => {
+        e.classList.add(`carousel-${classes[j]}`);
+      });
+      /* buttons */
+      const button = document.createElement('button');
+      if (!i) button.classList.add('selected');
+      button.addEventListener('click', () => {
+        block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
+        [...buttons.children].forEach((r) => r.classList.remove('selected'));
+        button.classList.add('selected');
+      });
+      buttons.append(button);
+    });
+    block.parentElement.append(buttons);
+  }
