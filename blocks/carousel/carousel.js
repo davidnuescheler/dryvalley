64d4ab71ef3dd1db759fee6dcf9322ab992acd27
David Nuescheler <david@nuescheler.ch>
2023-12-01 15:58:36 -0700

    chore: touchups

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,21 +1,42 @@
 export default function decorate(block) {
-    const buttons = document.createElement('div');
-    buttons.className = 'carousel-buttons';
-    [...block.children].forEach((row, i) => {
-      const classes = ['image', 'text'];
-      const children = [...row.children];
-      children.forEach((e, j) => {
-        e.classList.add(`carousel-${classes[j]}`);
-      });
-      /* buttons */
-      const button = document.createElement('button');
-      if (!i) button.classList.add('selected');
-      button.addEventListener('click', () => {
-        block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
-        [...buttons.children].forEach((r) => r.classList.remove('selected'));
+  const buttons = document.createElement('div');
+  buttons.className = 'carousel-buttons';
+  const slides = [...block.children];
+  
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const button = buttons.children[slides.indexOf(entry.target)];
+      if (entry.isIntersecting) {
         button.classList.add('selected');
-      });
-      buttons.append(button);
+      } else {
+        button.classList.remove('selected');
+
+      }
     });
-    block.parentElement.append(buttons);
-  }
+  }, {
+      root: block,
+      threshold: 0.5,
+  });
+
+  const scrollToSlide = (slideIndex) => {
+      const slide = slides[slideIndex];
+      block.scrollTo({ top: 0, left: slide.offsetLeft - slide.parentNode.offsetLeft, behavior: 'smooth' });
+  };
+
+  slides.forEach((row, i) => {
+    const classes = ['image', 'text'];
+    const children = [...row.children];
+    children.forEach((e, j) => {
+      e.classList.add(`carousel-${classes[j]}`);
+    });
+    /* buttons */
+    const button = document.createElement('button');
+    if (!i) button.classList.add('selected');
+    button.addEventListener('click', () => {
+      scrollToSlide(i);
+    });
+    buttons.append(button);
+    io.observe(row);
+  });
+  block.parentElement.append(buttons);
+}
